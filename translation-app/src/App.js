8d67bc5e3508c0c452a0ab36eef0aa030a7d0842
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-11-02 15:45:28 +0100

    App.js modified from functional component to class based component (because I want to learn things that way first, plus nice to see switching between these goes pretty quickly already)

--- translation-app/src/App.js
+++ translation-app/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
 
-    
-  const languages = [
+
+  languages = [
       { code: 'nl', name: 'Dutch' },
       { code: 'en', name: 'English' },
       { code: 'fr', name: 'French' },
@@ -12,36 +12,37 @@ function App() {
       { code: 'es', name: 'Spanish' }
   ];
 
-  const languageOptions = languages.map( language => {
+  languageOptions = this.languages.map( language => {
       return(
           <option value={language.code}>{language.name}</option>
       );
   });
 
-
-  return (
-    <div className="App">
-      <div className="languages">
-        <select className="language-select language-origin">
-            {languageOptions}
-        </select>
-        <button className="language-switch">switch button</button>
-        <select className="language-select language-result">
-            {languageOptions}
-        </select>
-      </div>
-      <div className="texts">
-        <textarea className="text-origin"/>
-        <textarea className="text-result" disabled="disabled"/>
-      </div>
-      <div className="api-ref">
-        <a href="http://translate.yandex.com/" target="_blank">Powered By Yandex</a>
-      </div>
-      <div className="reset">
-        <button className="reset-btn">Reset</button>
-      </div>
-    </div>
-  );
+  render() {
+      return (
+          <div className="App">
+            <div className="languages">
+              <select className="language-select language-origin">
+                  {this.languageOptions}
+              </select>
+              <button className="language-switch">switch button</button>
+              <select className="language-select language-result">
+                  {this.languageOptions}
+              </select>
+            </div>
+            <div className="texts">
+              <textarea className="text-origin"/>
+              <textarea className="text-result" disabled="disabled"/>
+            </div>
+            <div className="api-ref">
+              <a href="http://translate.yandex.com/" target="_blank">Powered By Yandex</a>
+            </div>
+            <div className="reset">
+              <button className="reset-btn">Reset</button>
+            </div>
+          </div>
+      );
+  }
 }
 
 export default App;

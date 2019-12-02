e11973f32310458082e85fb50212d00cf2d5f3d3
Wim Verpoorten <wim.verpoorten@hotmail.com>
2019-12-02 20:51:53 +0100

    styled components installed & applied to ResetButton

--- translation-app/src/components/ResetButton.js
+++ translation-app/src/components/ResetButton.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const ResetButton = (props) => (
-    <div className="reset">
-        <button className="reset-btn" onClick={props.clicked}>Reset</button>
-    </div>
-);
+const StyledResetButton = styled.button`
+    width: 100%;
+    padding: 0.75rem 1.5rem;
+    color: #ffb008;
+    background-color: #555555;
+    border: 1px solid #ffb008;
+    border-radius: 5px;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 1.4rem;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+  
+    &:hover {
+        color: #555555;
+        background-color: #c1fd00;
+        border: 1px solid #c1fd00;
+    }
+`;
+
+const ResetButton = (props) => {
+    return(
+        <StyledResetButton onClick={props.clicked}>Reset</StyledResetButton>
+    );
+};
 
 export default ResetButton;
